1fe023fca5b6cc4381d9b4d7a4d6257c27172ac1
Krissy Hales <kristinakfriel@gmail.com>
2019-07-18 18:51:03 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import routes from './routes'
 
 function App() {
   return (
     <div >
-
+    <Header />
+    {routes}
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+
+export default (
+    <Switch>
+        <Route exact path='/' component={Dashboard}/>
+        <Route path='/login' component={Login}/>
+        <Route path='/register' component={Register}/>
+    </Switch>
+);
