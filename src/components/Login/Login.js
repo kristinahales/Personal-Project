1f0e5e944591de376190b6cea61afab05eee175f
Krissy Hales <kristinakfriel@gmail.com>
2019-07-18 20:48:54 -0600

    changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -16,7 +16,6 @@ class Login extends React.Component {
     }
 
     handleChange(e) {
-        console.log(e.target.name)
         this.setState({
             [e.target.name]: e.target.value
         })
