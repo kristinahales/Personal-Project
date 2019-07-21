bb2ba60b09eadb3ddbccdf866982a5db716e03f6
Krissy Hales <kristinakfriel@gmail.com>
2019-07-21 12:05:03 -0600

    changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
         return (
             <header className='header-container'>
                 <h2 className='header-text'>Discover</h2>
-
+                <i id='hamburger' className="fas fa-bars"></i>
                 <nav className='nav-container'>
                     <span className='nav-links'>Home</span>
                     <span className='nav-links'>Orders</span>
