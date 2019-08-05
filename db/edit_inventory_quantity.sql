a9a9334a6766de473980b090253251f481b81526
Krissy Hales <kristinakfriel@gmail.com>
2019-08-05 11:29:33 -0600

    changed names

--- db/edit_inventory_quantity.sql
+++ db/edit_inventory_quantity.sql
@@ -3,6 +3,16 @@ SET quantity = $2
 WHERE inventory_id = $1
 AND user_id = $3;
 
+
+-- INSERT INTO user_inventory (inventory_id, quantity, user_id)
+-- VALUES ($1, $2, $3)
+-- ON CONFLICT  (inventory_id, quantity)
+-- DO 
+-- UPDATE
+-- SET quantity = $2
+-- WHERE inventory_id = $1
+-- AND user_id = $3; 
+
 SELECT * FROM inventory 
 LEFT JOIN user_inventory
 ON inventory.id = user_inventory.inventory_id
